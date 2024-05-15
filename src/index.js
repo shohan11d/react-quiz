bb9cfd3b335b7e0f15991907be76f95ea7505f50
Mohammad shohan <shohan11d@gmail.com>
2024-05-15 18:49:42 +0600

    data counter useReducer applied

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import App from "./App.js"
+import DateCounter from "./DateCounter.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
