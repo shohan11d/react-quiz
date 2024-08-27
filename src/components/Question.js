975ac0885d8fbd4c6af4d200a20dd0d7daee9892
shohan11d <shohan11d@gmail.com>
2024-08-27 06:46:16 +0600

    points functionality added

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,5 +1,12 @@
-function Question() {
-  return <div>Question  </div>;
+import Options from './Options';
+function Question({ question, dispatch, answer }) {
+  console.log(question);
+  return (
+    <div>
+      <h4>{question.question}</h4>
+      <Options question={question} dispatch={dispatch} answer={answer} />
+    </div>
+  );
 }
 
 export default Question;
